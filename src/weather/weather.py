e3c669a0d6d9d7a2b838603814cc3c4348da3c32
Admin <5567504@mail.ru>
2025-06-24 15:26:19 +0300

    BE: Доработал получение погоды

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -1,7 +1,6 @@
 from aiohttp import ClientSession
 from dotenv import load_dotenv
 import os
-from fastapi.encoders import jsonable_encoder
 
 load_dotenv()
 url = f"http://api.openweathermap.org/data/2.5/weather?q=санкт-петербург&lang=ru&units=metric&appid={os.getenv("WEATHER_TOKEN")}"
@@ -9,4 +8,8 @@ url = f"http://api.openweathermap.org/data/2.5/weather?q=санкт-петерб
 async def get_weather():
     async with ClientSession() as session:
         async with session.get(url=url) as response:
-            return await response.json()
+            result = await response.json()
+            weather = result["weather"][0]["main"]
+            temp = result["main"]["temp"]
+            return [{"weather": weather},
+                    {"temp": round(temp)}]
